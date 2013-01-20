b087cc6fa0e303b7b633e4939abf206b802a46c5
root <root@mint.(none)>
2013-01-20 00:50:23 -0800

    tabs->spaces

--- src/avl.c
+++ src/avl.c
@@ -34,123 +34,123 @@ avl_free_node(avl_tree *tree, avl_node *node)
 static avl_node *
 avl_rotate_2(avl_node **top, int dir)
 {
-	avl_node *B, *C, *D, *E;
-	B = *top;
-	D = B->child[dir];
-	C = D->child[1-dir];
-	E = D->child[dir];
+    avl_node *B, *C, *D, *E;
+    B = *top;
+    D = B->child[dir];
+    C = D->child[1-dir];
+    E = D->child[dir];
 
-	*top = D;
-	D->child[1-dir] = B;
-	B->child[dir] = C;
-	B->balance = AVL_NONE;
-	D->balance = AVL_NONE;
-	return E;
+    *top = D;
+    D->child[1-dir] = B;
+    B->child[dir] = C;
+    B->balance = AVL_NONE;
+    D->balance = AVL_NONE;
+    return E;
 }
 
 static avl_node *
 avl_rotate_3(avl_node **top, int dir, int third)
 {
-	avl_node *B, *F, *D, *C, *E;
-	B = *top;
-	F = B->child[dir];
-	D = F->child[1-dir];
-	C = D->child[1-dir];
-	E = D->child[dir];
-	*top = D;
-	D->child[1-dir] = B;
-	D->child[dir] = F;
-	B->child[dir] = C;
-	F->child[1-dir] = E;
-	D->balance = AVL_NONE;
-	B->balance = F->balance = AVL_NONE;
+    avl_node *B, *F, *D, *C, *E;
+    B = *top;
+    F = B->child[dir];
+    D = F->child[1-dir];
+    C = D->child[1-dir];
+    E = D->child[dir];
+    *top = D;
+    D->child[1-dir] = B;
+    D->child[dir] = F;
+    B->child[dir] = C;
+    F->child[1-dir] = E;
+    D->balance = AVL_NONE;
+    B->balance = F->balance = AVL_NONE;
 
-	if (third == AVL_NONE)
-		return NULL;
-	else if (third == dir) {
-		B->balance = 1-dir;
-		return E;
-	} else {
-		F->balance = dir;
-		return C;
-	}
+    if (third == AVL_NONE)
+        return NULL;
+    else if (third == dir) {
+        B->balance = 1-dir;
+        return E;
+    } else {
+        F->balance = dir;
+        return C;
+    }
 }
  
 static inline void 
 avl_rebalance_path(avl_tree *tree, avl_node *node, void *data, void *ctx)
 {
-	while (node && tree->comp(data, AVL_DATA(node, tree), ctx)) {
-		int step = (tree->comp(data, AVL_DATA(node, tree), ctx) > 0);
-		node->balance = step;
-		node = node->child[step];
-	}
+    while (node && tree->comp(data, AVL_DATA(node, tree), ctx)) {
+        int step = (tree->comp(data, AVL_DATA(node, tree), ctx) > 0);
+        node->balance = step;
+        node = node->child[step];
+    }
 }
 
 static inline void 
 avl_rebalance_insert(avl_tree *tree, avl_node **top, void *data, void *ctx)
 {
-	avl_node *node = *top;
-	int first, second, third;
-	if (AVL_BALANCED(node));
-	else if (node->balance != (first = (tree->comp(data, AVL_DATA(node, tree), ctx) > 0))) {
-		node->balance = AVL_NONE;
-		node = node->child[first]; 
-	} else if (first == (second = (tree->comp(data, AVL_DATA(node->child[first], tree), ctx) > 0))) {
-		node = avl_rotate_2(top, first);
-	} else {
-		node = node->child[first]->child[second];
-		if (tree->comp(data, AVL_DATA(node, tree), ctx) == 0) third = AVL_NONE;
-		else third = (tree->comp(data, AVL_DATA(node, tree), ctx) > 0);
-		node = avl_rotate_3(top, first, third);
-	}
-	avl_rebalance_path(tree, node, data, ctx);
+    avl_node *node = *top;
+    int first, second, third;
+    if (AVL_BALANCED(node));
+    else if (node->balance != (first = (tree->comp(data, AVL_DATA(node, tree), ctx) > 0))) {
+        node->balance = AVL_NONE;
+        node = node->child[first]; 
+    } else if (first == (second = (tree->comp(data, AVL_DATA(node->child[first], tree), ctx) > 0))) {
+        node = avl_rotate_2(top, first);
+    } else {
+        node = node->child[first]->child[second];
+        if (tree->comp(data, AVL_DATA(node, tree), ctx) == 0) third = AVL_NONE;
+        else third = (tree->comp(data, AVL_DATA(node, tree), ctx) > 0);
+        node = avl_rotate_3(top, first, third);
+    }
+    avl_rebalance_path(tree, node, data, ctx);
 }
 
 static inline void 
 avl_swap_del(avl_tree *tree, avl_node **ptarget, avl_node **proot, int dir)
 {
-	avl_node *target = *ptarget;
-	avl_node *node = *proot;
+    avl_node *target = *ptarget;
+    avl_node *node = *proot;
 
-	*ptarget = node;
-	*proot = node->child[1-dir];
-	node->child[AVL_LEFT] = target->child[AVL_LEFT];
-	node->child[AVL_RIGHT]= target->child[AVL_RIGHT];
-	node->balance = target->balance;
-	avl_free_node(tree, target);
+    *ptarget = node;
+    *proot = node->child[1-dir];
+    node->child[AVL_LEFT] = target->child[AVL_LEFT];
+    node->child[AVL_RIGHT]= target->child[AVL_RIGHT];
+    node->balance = target->balance;
+    avl_free_node(tree, target);
 }
 
 static inline avl_node **
 avl_rebalance_del(avl_tree *tree, avl_node **top, avl_node **ptarget, void *data, void *ctx)
 {
-	avl_node *target = *ptarget;
+    avl_node *target = *ptarget;
 
-	while (1) {
-		avl_node *node = *top;
-		int dir = (tree->comp(data, AVL_DATA(node, tree), ctx) > 0);
-		if (node->child[dir]==NULL) break;
-		if (AVL_BALANCED(node)) {
-			node->balance = 1-dir;
-		} else if (node->balance == dir) {
-			node->balance = AVL_NONE;
-		} else {
-			int second = node->child[1-dir]->balance;
-			if (second == dir) {
-				avl_rotate_3(top, 1-dir, node->child[1-dir]->child[dir]->balance);
-			} else if (second == AVL_NONE) {
-				avl_rotate_2(top, 1-dir);
-				node->balance = 1-dir;
-				(*top)->balance = dir;
-			} else {
-				avl_rotate_2(top, 1-dir);
+    while (1) {
+        avl_node *node = *top;
+        int dir = (tree->comp(data, AVL_DATA(node, tree), ctx) > 0);
+        if (node->child[dir]==NULL) break;
+        if (AVL_BALANCED(node)) {
+            node->balance = 1-dir;
+        } else if (node->balance == dir) {
+            node->balance = AVL_NONE;
+        } else {
+            int second = node->child[1-dir]->balance;
+            if (second == dir) {
+                avl_rotate_3(top, 1-dir, node->child[1-dir]->child[dir]->balance);
+            } else if (second == AVL_NONE) {
+                avl_rotate_2(top, 1-dir);
+                node->balance = 1-dir;
+                (*top)->balance = dir;
+            } else {
+                avl_rotate_2(top, 1-dir);
             }
-			if (node == target) {
-				ptarget = &(*top)->child[dir];
+            if (node == target) {
+                ptarget = &(*top)->child[dir];
             }
-		}
-		top = &node->child[dir];
-	}
-	return ptarget;
+        }
+        top = &node->child[dir];
+    }
+    return ptarget;
 }
  
 static int
@@ -248,26 +248,26 @@ avl_lookup(avl_tree *tree, void *data, void *ctx)
 {
     avl_node *node = tree->root;
 
-	while (node && tree->comp(data, AVL_DATA(node, tree), ctx)) {
-		int step = (tree->comp(data, AVL_DATA(node, tree), ctx) > 0);
-		node = node->child[step];
-	}
-	return node;
+    while (node && tree->comp(data, AVL_DATA(node, tree), ctx)) {
+        int step = (tree->comp(data, AVL_DATA(node, tree), ctx) > 0);
+        node = node->child[step];
+    }
+    return node;
 }
 
 int 
 avl_insert(avl_tree *tree, void *data, void *ctx)
 {
     avl_node **proot = &tree->root;
-	avl_node *node = *proot;
-	avl_node **top = proot;
-	while (node && tree->comp(data, AVL_DATA(node, tree), ctx)) {
-		int step = (tree->comp(data, AVL_DATA(node, tree), ctx) > 0);
-		if (!AVL_BALANCED(node)) top = proot;
-		proot = &node->child[step];
-		node = *proot;
-	}
-	if (node) return 0;
+    avl_node *node = *proot;
+    avl_node **top = proot;
+    while (node && tree->comp(data, AVL_DATA(node, tree), ctx)) {
+        int step = (tree->comp(data, AVL_DATA(node, tree), ctx) > 0);
+        if (!AVL_BALANCED(node)) top = proot;
+        proot = &node->child[step];
+        node = *proot;
+    }
+    if (node) return 0;
 
     if (tree->opts & AVL_INTR) {
         node = (avl_node *) data;
@@ -285,27 +285,27 @@ int
 avl_remove(avl_tree *tree, void *data, void *ctx)
 {
     avl_node **proot = &tree->root;
-	avl_node  *node = *proot;
-	avl_node **top = proot;
-	avl_node **ptarget = NULL;
-	int dir;
-	
-	while (node) {
-		dir = (tree->comp(data, AVL_DATA(node, tree), ctx) > 0);
-		if (tree->comp(data, AVL_DATA(node, tree), ctx) == 0)
-			ptarget = proot;
-		if (node->child[dir] == NULL)
-			break;
-		if (AVL_BALANCED(node) || (node->balance == (1-dir) && AVL_BALANCED(node->child[1-dir]))) top = proot;
-		proot = &node->child[dir];
-		node = *proot;
-	}
-	if (!ptarget) return 0;
+    avl_node  *node = *proot;
+    avl_node **top = proot;
+    avl_node **ptarget = NULL;
+    int dir;
+    
+    while (node) {
+        dir = (tree->comp(data, AVL_DATA(node, tree), ctx) > 0);
+        if (tree->comp(data, AVL_DATA(node, tree), ctx) == 0)
+            ptarget = proot;
+        if (node->child[dir] == NULL)
+            break;
+        if (AVL_BALANCED(node) || (node->balance == (1-dir) && AVL_BALANCED(node->child[1-dir]))) top = proot;
+        proot = &node->child[dir];
+        node = *proot;
+    }
+    if (!ptarget) return 0;
 
-	ptarget = avl_rebalance_del(tree, top, ptarget, data, ctx);
-	avl_swap_del(tree, ptarget, proot, dir);
+    ptarget = avl_rebalance_del(tree, top, ptarget, data, ctx);
+    avl_swap_del(tree, ptarget, proot, dir);
     tree->size--;
-	return 1;
+    return 1;
 }
 
 int
